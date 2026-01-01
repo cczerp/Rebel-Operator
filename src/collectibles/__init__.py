4e67a910be7b06821b9846c4d14fb7f7ef62a9a0
cczerp <cczerp@gmail.com>
2026-01-01 15:14:05 -0600

    wired up enhanced analyzer

--- src/collectibles/__init__.py
+++ src/collectibles/__init__.py
@@ -2,10 +2,12 @@
 
 from .recognizer import CollectibleRecognizer, identify_collectible
 from .attribute_detector import AttributeDetector, detect_attributes
+from .enhanced_scanner import EnhancedScanner
 
 __all__ = [
     "CollectibleRecognizer",
     "identify_collectible",
     "AttributeDetector",
     "detect_attributes",
+    "EnhancedScanner",
 ]
